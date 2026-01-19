75f44a61595e2a4dc667cbc8648b0bd404c3b2f9
Aleksa Piperski <mr.alexpiperski@gmail.com>
2026-01-19 14:39:55 +0100

    Added hashing for user password sooner

--- services/userService.js
+++ services/userService.js
@@ -1,12 +1,10 @@
-const bcrypt = require("bcryptjs");
 const Login = require("../model/loginModel");
 
 async function registerPendingUser(pendingUser) {
   if (!pendingUser) throw new Error("No pending user data");
 
   const { email, surname, lastname, password } = pendingUser;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  await Login.addUser(email, hashedPassword, surname, lastname);
+  await Login.addUser(email, password, surname, lastname);
 }
 
 module.exports = { registerPendingUser };
