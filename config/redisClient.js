799de1cf0feaa69a848b6ee1b13001bb10a90c33
Aleksa Piperski <mr.alexpiperski@gmail.com>
2026-08-17 14:23:53 +0200

    Removed unecessery redis creation, imporved cookie

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,7 +1,7 @@
 const Redis = require("redis");
 
 const redisClient = Redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL,
 });
 
 // handle connection errors
